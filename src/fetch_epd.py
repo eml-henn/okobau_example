37a4fbbdeeef1ba7f39fdffaf88dc8d9418177c6
eml-henn <emiliano.lupo@henn.com>
2024-02-21 17:39:09 +0100

    small updates

--- src/fetch_epd.py
+++ src/fetch_epd.py
@@ -12,9 +12,8 @@ def get_epd_by_id(uuid: str):
 
 if __name__ == '__main__':
 
-    folder = get_folder(__file__, "epdx_data")    
-
 #    epd_id = "8be9edb5-c5b9-4be1-bfb8-b096f24a183b"
 #    get_epd_by_id(epd_id)
     epd_id = "c23b2987-776d-4d55-91c7-5f2a0f2c50f1"
     get_epd_by_id(epd_id)
+
